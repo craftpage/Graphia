b594a9dd39cb514d1798cee99a10460d902c3c28
craftpage <craftpage@gmail.com>
2020-08-29 22:17:01 +0900

    first commit

---
+++ graph.js
@@ -0,0 +1,93 @@
+﻿const draw = () => {
+	
+	let data = {
+		width: 300,
+		height: 300,
+		xStart : parseFloat(document.getElementById('xS').value),
+		xEnd : parseFloat(document.getElementById('xE').value),
+		yStart : parseFloat(document.getElementById('yS').value),
+		yEnd : parseFloat(document.getElementById('yS').value)
+	}
+
+	const canvas = document.getElementById("Graph")
+	const ctx = canvas.getContext('2d')
+	ctx.beginPath()	
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.font = "10pt Arial"
+	ctx.fillText("Graphia", 300, 280)
+	ctx.fillText('f(x)', 25, 10)
+
+	let formula = function(x){
+		return eval(document.getElementById('formula').value)
+	}
+
+	let scaleLength = function(start,end){
+		return Math.abs(start) + Math.abs(end);
+	}
+
+	let xHi= data.width / Math.abs(data.xStart) * data.xStart/scaleLength(data.xStart, data.xEnd);
+	let yHi= data.height * data.yStart/scaleLength(data.yStart,data.yEnd)
+
+	let x, View;
+	let HI = scaleLength(data.xStart,data.xEnd)/scaleLength(data.yStart,data.yEnd);
+
+	//目盛り
+	if(data.xStart < data.xEnd && data.yStart < data.yEnd || "\d"){
+		let x = data.width / 4
+		let h = data.height
+		let y = h + 10
+		let XNDiv = []
+		let YNDiv = []
+		for (let p = 0; p <= 4; p++) {
+			XNDiv[p] = data.xStart+scaleLength(data.xStart,data.xEnd)*p/4
+			ctx.fillText(XNDiv[p], x, data.height + 20)
+			x += data.width / 4
+
+			YNDiv[p] = data.yStart+scaleLength(data.yStart, data.yEnd)*(4-p)/4;
+			ctx.fillText(YNDiv[p], 0, y);
+			y -= data.height / 4
+		}
+		ctx.fillText('x', x, data.height + 20)
+	} else {
+		document.getElementById("Error").innerHTML = "<p>Error</p>";
+	}
+	
+
+	ctx.strokeRect(data.width / 4, 0, data.width, data.height)
+	for(let e = 0; e <= data.width; e++) {
+ 		x = data.xStart+scaleLength(data.xStart,data.xEnd)*e/data.width 
+		View = yHi+xHi*formula(x)*HI;
+		Plot(e);
+	}
+
+	function Plot(e){
+		e += data.width / 4
+		if (-data.height <= View && View<=0){
+			ctx.lineTo(e, data.height+View);
+		}
+		ctx.stroke()
+	}
+
+	ctx.closePath()
+}
+
+function imgSave(){
+	canvas = document.getElementById('Graph')
+    base64 = canvas.toDataURL("image/png")
+    document.getElementById("imgSave").href = base64
+}
+
+function erase (){
+	const canvas = document.getElementById("Graph")
+	const ctx = canvas.getContext('2d')
+	ctx.beginPath()
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.closePath()
+	document.getElementById('formula').value = ''
+}
+
+window.onload = () => {
+	document.getElementById('draw').addEventListener('click', draw)
+	document.getElementById('erase').addEventListener('click', erase)
+	document.getElementById('imgSave').addEventListener('click', imgSave)
+}
---
+++ interface.js
@@ -0,0 +1,55 @@
+﻿function A1(o){
+	document.getElementById(o).style.display="block";
+	switch(o){
+		case 1:	A2(2);A2(4);A2(8);	break;
+		case 2:	A2(1);A2(4);A2(8);	break;
+		case 4:	A2(1);A2(2);A2(8);	break;
+		case 8:	A2(1);A2(2);A2(4);	break;
+	}
+}
+
+function A2(o){
+	document.getElementById(o).style.display="none";
+}
+
+function D(A,B){
+var Short = document.getElementById("formula");
+	switch(A*10+B){
+		case 0: Short.value += "0" ; break;
+		case 1: Short.value += "1" ; break;
+		case 2: Short.value += "2" ; break;
+		case 3: Short.value += "3" ; break;
+		case 4: Short.value += "4" ; break;
+		case 5: Short.value += "5" ; break;
+		case 6: Short.value += "6" ; break;
+		case 7: Short.value += "7" ; break;
+		case 8: Short.value += "8" ; break;
+		case 9: Short.value += "9" ; break;
+
+		case 21:Short.value +="Math.E";break;
+		case 22:Short.value +="Math.PI";break;
+		case 23:Short.value +="x";break;
+		case 24:Short.value +=".";break;
+
+		case 31: Short.value += "Math.sin(x)" ; break;
+		case 32: Short.value += "Math.cos(x)" ; break;
+		case 33: Short.value += "Math.tan(x)" ; break;
+		case 34: Short.value += "Math.asin(x)" ; break;
+		case 35: Short.value += "Math.acos(x)" ; break;
+		case 36: Short.value += "Math.atan(x)" ; break;
+		
+		case 41: Short.value += "+" ; break;
+		case 42: Short.value += "-" ; break;
+		case 43: Short.value += "*" ; break;
+		case 44: Short.value += "/" ; break;
+
+		case 51: Short.value += "Math.sqrt(x)";break;
+		case 52: Short.value += "Math.pow(,)";break;
+		case 53: Short.value += "Math.exp(x)";break;
+		case 54: Short.value += "Math.log(x)";break;
+
+		case 70:Short.value += "Math.round()";break;
+		case 71:Short.value += "Math.ceil()";break;
+		case 72:Short.value += "Math.floor()";break;
+	}
+}
